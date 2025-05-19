b37311df4d38d9617274c6bb23c4ddbc06b654c8
MG <mguacapina@tecnologicoismac.edu.ec>
2025-05-19 12:12:45 -0500

    Test

---
+++ src/test/java/com/distribuida/test/FacturaTest.java
@@ -0,0 +1,19 @@
+package com.distribuida.test;
+
+import org.distribuida.entities.Factura;
+import org.distribuida.entities.cliente;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Date;
+
+public class FacturaTest {
+
+    private Factura factura;
+
+    @BeforeEach
+    public void setup(){
+
+
+    }
+
+}
